65c5cd85d1b7fe2607b7dab59bc09cdecdaa5243
yuki0920 <ywsep20@gmail.com>
2021-04-19 08:40:50 +0900

    Extract Menu component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,7 @@
   <Nav />
   <div class="container-fluid">
     <div class="row">
-      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-        <div class="position-sticky pt-3">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                ダッシュボード
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+      <Menu />
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="table-responsive">
           <table class="table table-striped table-sm">
@@ -44,10 +33,12 @@
 
 <script>
 import Nav  from '@/components/Nav'
+import Menu  from '@/components/Menu'
 
 export default {
   components: {
-    Nav
+    Nav,
+    Menu
   }
 }
 </script>
