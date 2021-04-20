74876cf5b350a8ef43759c5a752bedc03648e921
yuki0920 <ywsep20@gmail.com>
2021-04-20 09:02:42 +0900

    Add signin form

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,6 +1,15 @@
 <template>
-  <h1>{{count}}</h1>
-  <input type="number" v-model="count">
+  <main class="form-signin">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please register</h1>
+      <input class="form-control" placeholder="First Name" required>
+      <input class="form-control" placeholder="Last Name" required>
+      <input type="email" class="form-control" placeholder="Email" required>
+      <input type="password" class="form-control" placeholder="Password" required>
+      <input type="password" class="form-control" placeholder="Password Confirm" required>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+    </form>
+  </main>
 </template>
 
 <script>
@@ -8,12 +17,50 @@
 import {ref} from 'vue'
 export default {
   name: "Register",
-  setup() {
-    const count = ref(0);
-
-    return {
-      count
-    }
-  },
 }
 </script>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
