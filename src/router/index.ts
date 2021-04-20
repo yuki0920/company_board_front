74c7cdb3e3e550f12755912910b208b86664753f
yuki0920 <ywsep20@gmail.com>
2021-04-20 09:03:12 +0900

    Add Wrapper component

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,18 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Dashboard from '@/pages/Dashboard.vue'
 import Users from '@/pages/Users.vue'
 import Register from '@/pages/Register.vue'
+import Wrapper from '@/pages/Wrapper.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {path: '', component: Dashboard},
-  {path: '/users', component: Users},
-  {path: '/register', component: Register},
+  { path: '/register', component: Register },
+  {
+    path: '',
+    component: Wrapper,
+    children: [
+      { path: '', component: Dashboard },
+      { path: '/users', component: Users },
+    ]
+  },
 ]
 
 const router = createRouter({
