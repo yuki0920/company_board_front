d8892351f0aae8d7c706a07ce8455bcb7219538e
yuki0920 <ywsep20@gmail.com>
2021-04-25 11:58:18 +0900

    Chart for sales

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,9 +1,50 @@
 <template>
-  Dashboard
+  <h2>Daily Sales</h2>
+  <div id="chart"></div>
 </template>
 
-<script>
+<script lang="ts">
+import {onMounted} from 'vue'
+import axios from 'axios'
+import * as c3 from 'c3'
+
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  setup() {
+    onMounted(async () => {
+      const chart = c3.generate({
+        // バインディング対象のID
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [
+            ['x'],
+            ['Sales']
+          ],
+          types: {
+            // バーチャート
+            Sales: 'bar'
+          }
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d'
+            }
+          }
+        }
+      })
+
+      const {data} = await axios.get('chart')
+      // dataの要素を1つずつcolumns に追加する
+      chart.load({
+        columns: [
+          ['x', ...data.map((r: any) => r.date)],
+          ['Sales', ...data.map((r: any) => r.sum)]
+        ]
+      })
+    })
+  }
 }
 </script>
